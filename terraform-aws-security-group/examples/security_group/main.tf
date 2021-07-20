054355a3b40f742a013a604900fd2b0f604cc88c
Richard Augenti <richard.augenti@rivia.io>
2021-07-19 19:23:22 -0700

    update

--- terraform-aws-security-group/examples/security_group/main.tf
+++ terraform-aws-security-group/examples/security_group/main.tf
@@ -10,8 +10,6 @@ terraform {
 # Configure the AWS Provider
 provider "aws" {
   region = "us-east-1"
-  access_key = "AKIA22RS3MD6FZ7L4SEK"
-  secret_key = "1tQzNseQJpKCJR4pMnOFEvKRjxkhpEsebuGSSI+W"
 }
 
 module "map" {
